b17588fcf143974c00977bef7cbbad3f2c8588dc
vladh <vlad.hagai@mai.ru>
2024-01-07 23:32:28 +0300

    тихонько понимаю

--- mysite/women/views.py
+++ mysite/women/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("страница приложения women")
 
-def categories(request):
-    return HttpResponse("<h1>Статьи по категориям</h1>")
+def categories(request, catid):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
