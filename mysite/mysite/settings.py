dd94c9d1c0f72a6e3ccf1d3f4d7eaf7a64754cac
vladh <vlad.hagai@mai.ru>
2024-01-09 22:04:18 +0300

    TIK_002: отладка страницы NotFound разобрался в мелких фичах

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^k$_zo$*0+ty=et3=vmknno*uh!!8b=@$o-mzo76z74sp(=ktf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- mysite/women/views.py
+++ mysite/women/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
 
 def index(request):
     return HttpResponse("страница приложения women")
 
 def categories(request, catid):
+    if request.POST:
+        print(request.POST)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
